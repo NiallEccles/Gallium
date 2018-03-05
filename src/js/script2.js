dbf582e6a47a2ff54f1a2ff578626a15c83c878e
Niall Eccles <n.eccles14@gmail.com>
2018-03-05 22:08:37 +0000

    Added in images to gallery. Added main image.

--- src/js/script2.js
+++ src/js/script2.js
@@ -1,2 +1,13 @@
-//Second log
-console.log('This is the second log');
+var current = document.querySelector('#current');
+var images = document.querySelectorAll('.gallery-images img');
+var opacity = 0.4;
+
+images.forEach(function(img){
+    img.addEventListener('click', function(e){
+        images.forEach(function(img){
+            img.style.opacity = 1;
+        })
+        current.src = e.target.src;
+        e.target.style.opacity = opacity;
+    })
+});
